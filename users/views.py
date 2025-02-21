2110159af19629a3c3110edf1fc9d5ca1295d79f
degregor69 <raphael.degregorio@gmail.com>
2025-02-21 09:03:08 +0100

    signup and login forms created

--- users/views.py
+++ users/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.contrib.auth import login, logout
+from django.contrib.auth.views import LoginView
+from django.shortcuts import render, redirect
+from .forms import SignUpForm
 
-# Create your views here.
+def signup_view(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("home")
+    else:
+        form = SignUpForm()
+    return render(request, "users/signup.html", {"form": form})
+
+class CustomLoginView(LoginView):
+    template_name = "users/login.html"
+
+def logout_view(request):
+    logout(request)
+    return redirect("home")
