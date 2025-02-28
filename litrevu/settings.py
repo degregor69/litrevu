081c6ee80ff2953e1592721fafed09174005b2d4
degregor69 <raphael.degregorio@gmail.com>
2025-02-28 09:57:46 +0100

    created reviews model

--- litrevu/settings.py
+++ litrevu/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "users",
-    "follows"
+    "follows",
+    "tickets",
+    "reviews"
 ]
 
 MIDDLEWARE = [
