bd3971e40888034839a4809d38d115bc9d61562d
degregor69 <raphael.degregorio@gmail.com>
2025-02-28 08:50:15 +0100

    created signin and signup success pages

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from django.contrib.auth.views import LogoutView
-from .views import signup_view, CustomLoginView
+from .views import signup_view, CustomLoginView, signup_success, signin_success
 
 urlpatterns = [
     path("signup/", signup_view, name="signup"),
     path("login/", CustomLoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(), name="logout"),
+    path("signup-success/", signup_success, name="signup_success"),
+    path("signin-success/", signin_success, name="signin_success"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import login, logout
 from django.contrib.auth.views import LoginView
 from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+
 from .forms import SignUpForm
 
 def signup_view(request):
@@ -9,7 +11,7 @@ def signup_view(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect("home")
+            return signup_success(request)
     else:
         form = SignUpForm()
     return render(request, "users/signup.html", {"form": form})
@@ -17,6 +19,18 @@ def signup_view(request):
 class CustomLoginView(LoginView):
     template_name = "users/login.html"
 
+    success_url = reverse_lazy("signin_success")
+
+    def get_success_url(self):
+        return self.success_url
+
 def logout_view(request):
     logout(request)
     return redirect("home")
+
+
+def signup_success(request):
+    return render(request, "users/signup_success.html")
+
+def signin_success(request):
+    return render(request, "users/signin_success.html")
