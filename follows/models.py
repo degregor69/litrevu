97cc2021c0e54603b541ad387a9bce526d9e39c6
degregor69 <raphael.degregorio@gmail.com>
2025-02-28 08:55:43 +0100

    created follows model

---
+++ follows/models.py
@@ -0,0 +1,20 @@
+from django.conf import settings
+from django.db import models
+
+class UserFollows(models.Model):
+    user = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="following"
+    )
+    followed_user = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="followed_by"
+    )
+
+    class Meta:
+        unique_together = ("user", "followed_user")  # Impossible for one user to follow the same user twice
+
+    def __str__(self):
+        return f"{self.user} suit {self.followed_user}"
