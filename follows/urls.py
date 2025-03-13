fae5932a2ff339daad5c450849ffadd2582cee80
degregor69 <raphael.degregorio@gmail.com>
2025-03-13 09:55:51 +0100

    following page done

--- follows/urls.py
+++ follows/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import follows_index, unfollow
+from .views import follows_index, unfollow, follow
 
 urlpatterns = [
-    path('', follows_index, name='following_list'),
-    path('unfollow/<int:follow_id>/', unfollow , name='unfollow')
+    path('', follows_index, name='follows_index'),
+    path("unfollow/<int:follow_id>/", unfollow, name="unfollow"),
+    path('follow/', follow, name='follow')
 ]
--- follows/views.py
+++ follows/views.py
@@ -1,16 +1,32 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from .models import UserFollows
+from users.models import CustomUser
 
 @login_required
 def follows_index(request):
-    followers = UserFollows.objects.filter(followed_user=request.user)  # Ceux qui suivent l'utilisateur
-    following = UserFollows.objects.filter(user=request.user)  # Ceux que l'utilisateur suit
+    all_users = CustomUser.objects.exclude(id=request.user.id).filter(is_superuser=False)
+    followers = UserFollows.objects.filter(followed_user=request.user)
+    following = UserFollows.objects.filter(user=request.user)
 
-    return render(request, "follows_index.html", {"followers": followers, "following": following})
+    following_ids = following.values_list("followed_user__id", flat=True)
+    users_to_follow = all_users.exclude(id__in=following_ids)
+    return render(request, "follows_index.html", {"users_to_follow": users_to_follow,"followers": followers, "following": following})
 
 @login_required
 def unfollow(request, follow_id):
     follow = get_object_or_404(UserFollows, id=follow_id, user=request.user)
     follow.delete()
-    return redirect("follows_index.html")
+    return redirect("follows_index")
+
+
+@login_required
+def follow(request):
+    user_id = request.POST.get('user_id')
+    if user_id:
+        user_to_follow = get_object_or_404(CustomUser, id=user_id)
+
+        if not UserFollows.objects.filter(user=request.user, followed_user=user_to_follow).exists():
+            UserFollows.objects.create(user=request.user, followed_user=user_to_follow)
+
+    return redirect('follows_index')
