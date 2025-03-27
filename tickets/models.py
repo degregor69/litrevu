07d88423063230ba5efce11475b73d7dec942cd0
degregor69 <raphael.degregorio@gmail.com>
2025-03-27 21:21:47 +0100

    added verbose name to ticket model

--- tickets/models.py
+++ tickets/models.py
@@ -2,13 +2,13 @@ from django.conf import settings
 from django.db import models
 
 class Ticket(models.Model):
-    title = models.CharField(max_length=128)
-    description = models.TextField(max_length=2048, blank=True)
+    title = models.CharField(max_length=128, verbose_name="Titre")
+    description = models.TextField(max_length=2048, blank=True, verbose_name="Description")
     user = models.ForeignKey(
         to=settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE
     )
-    image = models.ImageField(null=True, blank=True, upload_to="ticket_images/")
+    image = models.ImageField(null=True, blank=True, upload_to="ticket_images/", verbose_name="Image")
     time_created = models.DateTimeField(auto_now_add=True)
 
     def has_user_review(self, user):
